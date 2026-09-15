74b0e7d981b78f712b5eb38ae3bdcf63dc0802ce
Daniel Nachun <daniel.nachun@gmail.com>
2026-09-14 19:12:09 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     microsoft-${PKG_NAME}-${PKG_VERSION}.tgz
@@ -13,6 +14,13 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/compose-language-service << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@microsoft/compose-language-service/bin/docker-compose-langserver "\$@"
+EOF
+chmod +x ${PREFIX}/bin/compose-language-service
+
 tee ${PREFIX}/bin/compose-language-service.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\compose-language-service %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@microsoft\compose-language-service\bin\docker-compose-langserver %*
 EOF
